4602bd68b7fdd3198747f86632e49f74430254c7
Elena Perijoska <perijoskaelena@iwconnect.com>
2021-09-01 23:35:01 +0200

    innitial commit

---
+++ homework1-Filter&Map/main.js
@@ -0,0 +1,29 @@
+   import * as data from "/peopledata.js";
+
+   const peopleEndWithA= data.peopleArray.filter(x=>x.name.endsWith("a"));
+    console.log(peopleEndWithA);
+    peopleEndWithA.forEach((x)=>{
+        console.log(x);
+    });    
+    function renderPeople (peopleData) {
+    let gridList= document.getElementById("gridList");
+    console.log(peopleData);
+          peopleData.forEach(x => {          
+          let listItem= `<li class="cards_item">
+                <div class="card">
+                    <div class="card_image"><img src="${x.image}"}></div>
+                    <div class="card_content">
+                        <h2 class="card_title">Name: ${x.name}</h2>
+                        <p class="card_text">Age: ${x.age}</p>
+                        <p class="card_text">Role: ${x.role}</p>
+                        <button class="btn card_btn">Contact</button>
+                    </div>
+                </div>
+            </li>`
+          console.log(x.name);
+          gridList.innerHTML+=listItem;
+      });
+    }
+    renderPeople(peopleEndWithA);
+        
+    

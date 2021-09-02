81d9a378e54eff9601bd2c8463b680fddba6e85d
Elena Perijoska <perijoskaelena@iwconnect.com>
2021-09-02 17:41:03 +0200

    removed unnec. code

--- homework1-Filter&Map/filter.js
+++ homework1-Filter&Map/filter.js
@@ -5,28 +5,8 @@
     peopleEndWithA.forEach((x)=>{
         console.log(x);
     }); 
-     let gridList= document.getElementById("gridList");     
-
+     let gridList= document.getElementById("gridList");   
     let renderPeople =(peopleData) => { 
-     removeAllChildNodes(gridList);
-        console.log(peopleData);
-          peopleData.forEach(x => {          
-          let listItem= `<li class="cards_item">
-                <div class="card">
-                    <div class="card_image"><img src="${x.image}"}></div>
-                    <div class="card_content">
-                        <h2 class="card_title">Name: ${x.name}</h2>
-                        <p class="card_text">Age: ${x.age}</p>
-                        <p class="card_text">Role: ${x.role}</p>
-                        <button class="btn card_btn">Contact</button>
-                    </div>
-                </div>
-            </li>`
-          console.log(x.name);
-          gridList.innerHTML+=listItem;
-      });
-    }
-    let  renderPeopleA= (peopleData)=> {
      removeAllChildNodes(gridList);
         console.log(peopleData);
           peopleData.forEach(x => {          
@@ -49,10 +29,14 @@
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
-}
-  document.getElementById("all-names").addEventListener("click", ()=>{renderPeople(data.peopleArray)});
+  }
+        document.getElementById("all-names").addEventListener("click", ()=>{renderPeople(data.peopleArray)});
+        document.getElementById("names-A").addEventListener("click", ()=>{ renderPeople(peopleEndWithA)});
   
-  document.getElementById("names-A").addEventListener("click", ()=>{ renderPeopleA(peopleEndWithA)});
-    
+
+ 
+
+  
+ 
         
     
--- homework1-Filter&Map/peopledata.js
+++ homework1-Filter&Map/peopledata.js
@@ -2,7 +2,7 @@ export const peopleArray= [
 {"name":"Elena", "age":27, "role":"Backend Web Developer", "image":"https://www.mckinsey.com/~/media/mckinsey/careers%20redesign/meet%20our%20people/main/careersr2_meet-our-people_thumb_1536x1536.jpg" },
 {"name":"Marija", "age":36, "role":"CEO", "image":"https://ichef.bbci.co.uk/news/976/cpsprodpb/DBEA/production/_117389265_raya-online-use-fullcomp_publicity_raya001_6k_709film.0042_rgb.jpg"},
 {"name":"Bosilka", "age":24, "role":"DevOps","image":"https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg?width=982&height=726&auto=webp&quality=75"},
-{"name":"Stavre", "age":30, "role":"SysAdmin","image":"https://www.g20.org/wp-content/uploads/2021/01/people.jpg"},
+{"name":"Ines", "age":30, "role":"SysAdmin","image":"https://www.g20.org/wp-content/uploads/2021/01/people.jpg"},
 {"name":"Zoki", "age":25, "role":"Human Resources","image":"https://www.tubefilter.com/wp-content/uploads/2019/11/dobrik-people.jpg"},
 {"name":"Nikola", "age":32, "role":"Human Resources","image":"https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/06/15/Chris-Pratt.jpg?width=982&height=726&auto=webp&quality=75"}
 ];

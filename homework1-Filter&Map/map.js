4ab510265d53689dbf715eb19669f28cf19ef55b
Elena Perijoska <perijoskaelena@iwconnect.com>
2021-09-13 16:27:43 +0200

    Added navbar homework

--- homework1-Filter&Map/map.js
+++ homework1-Filter&Map/map.js
@@ -1,5 +1,5 @@
 const numbersArray= [1, 2, 3, 4, 5]
-const incrementedNumbers = numbersArray.map(n=>n*10);
+const incrementedNumbers = numbersArray.map(n=>n+10);
 console.log(incrementedNumbers);
 incrementedNumbers.forEach((n) => {
     console.log(n);
